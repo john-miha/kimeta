bae80cf575e1c0f5f253a491b9b970d1ef665cd4
john-miha <sphyrna42@gmail.com>
2022-05-12 18:22:53 +0900

    Add mybest column to charts

--- db/migrate/20220512092217_add_mybest_to_charts.rb
+++ db/migrate/20220512092217_add_mybest_to_charts.rb
@@ -1,5 +1,5 @@
 class AddMybestToCharts < ActiveRecord::Migration[6.1]
   def change
-    add_column :charts, :mybest, :bool
+    add_column :charts, :mybest, :int
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_11_105104) do
+ActiveRecord::Schema.define(version: 2022_05_12_092217) do
 
   create_table "charts", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_05_11_105104) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "summarycomment"
+    t.integer "mybest"
   end
 
   create_table "evaluations", charset: "utf8mb4", force: :cascade do |t|
